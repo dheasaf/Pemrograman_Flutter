244b3b817243e4f61365b29702396f0adbe81abb
Dhea safira <127371134+dheasaf@users.noreply.github.com>
2023-10-03 23:24:32 +0700

    upload video ke-5 "menambahkan style dan font"

--- projek2/lib/main.dart
+++ projek2/lib/main.dart
@@ -15,7 +15,24 @@ class MyApp extends StatelessWidget{
           title: Text("My Apps")
         ),
         body: Center(
-          child: Text("HALLO"),
+          child: Text(
+              "ajdnhenfkcwefhkawdnankxala xajssdklwekrw3 dkasxiad qjqwf ajx jdbwdiewjf axerkquwd dstsje dhsywnsbh shsgts",
+            maxLines: 2,
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              backgroundColor: Colors.cyan,
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'DelaGothicOne',
+              decorationStyle: TextDecorationStyle.dotted,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.cyanAccent,
+              decorationThickness: 2,
+
+
+            ),
+          ),
         ),
       ),
     );
