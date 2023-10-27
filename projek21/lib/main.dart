a805a55ba23d4f6048fab1f765fa365bf1fd2a19
Dhea safira <127371134+dheasaf@users.noreply.github.com>
2023-10-27 22:08:43 +0700

    upload tugas video ke-24 "drawer"

---
+++ projek21/lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Drawer"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 150,
+              color: Colors.red,
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Menu Pilihan",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => PageSatu(),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.home,
+                size: 35,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => SettingPage(),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.settings,
+                size: 35,
+              ),
+              title: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "some text here!",
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Satu"),
+      ),
+      body: Center(
+        child: Text(
+          "Halaman Satu",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Center(
+        child: Text(
+          "Settings Page",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
