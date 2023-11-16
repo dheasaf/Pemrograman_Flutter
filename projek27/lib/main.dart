14b714ad7243a841adccf15d3d01fc0af6828c43
Dhea safira <127371134+dheasaf@users.noreply.github.com>
2023-11-16 10:42:44 +0700

    upload tugas video ke-31 "Fitted Box"

---
+++ projek27/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fitted Box"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.indigo,
+            width: 300,
+            height: 100,
+            child: FittedBox(
+                alignment: Alignment.centerRight,
+                child: Image.network("https://picsum.photos/200/300")),
+          ),
+        ),
+      ),
+    );
+  }
+}
